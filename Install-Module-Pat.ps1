90a9471151791019c6754914be178814bbf59296
LongjunXU <xulongjun.fr@gmail.com>
2023-04-17 02:45:20 +0200

    test SYSTEM_ACCESSTOKEN

--- Install-Module-Pat.ps1
+++ Install-Module-Pat.ps1
@@ -1,7 +1,7 @@
 
-$patToken = "$('vep3qcbpsrhmp5s6wa4zo4rgbxpexrk7rzgzqkpj5pgtwbjs3erq')" | ConvertTo-SecureString -AsPlainText -Force
+$patToken = $env:SYSTEM_ACCESSTOKEN | ConvertTo-SecureString -AsPlainText -Force
 
-$credsAzureDevopsServices = New-Object System.Management.Automation.PSCredential("longjun xu", $patToken)
+$credsAzureDevopsServices = New-Object System.Management.Automation.PSCredential($env:SYSTEM_ACCESSTOKEN, $patToken)
 
 Register-PSRepository -Name "PowershellAzureDevopsServices" -SourceLocation "https://pkgs.dev.azure.com/xulongjunfr//_packaging/psModule/nuget/v2" -InstallationPolicy Trusted -Credential $credsAzureDevopsServices
 
