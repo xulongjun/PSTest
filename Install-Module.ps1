8406498233d37df5185e8bfb795f6bcfd566c102
LongjunXU <xulongjun.fr@gmail.com>
2023-04-17 23:49:25 +0200

    test

--- Install-Module.ps1
+++ Install-Module.ps1
@@ -17,17 +17,23 @@ Write-Host "pathNuget : $pathNuget"
 # Uninstall-Module -Name "KpInfo"  -AllVersions
 # Remove-Module -Name "KpInfo"
 
-# Ensure the module is not already installed
-if (!(Get-Module -ListAvailable -Name $moduleName)) {
-    # Save the NuGet package containing the module to a temporary folder
-    $tempFolder = New-Item -ItemType Directory -Path (Join-Path $env:TEMP ([System.Guid]::NewGuid().ToString()))
-    $nugetConfigPath = "$pathNuget\nuget.config"
-    & "nuget.exe" install $moduleName -ExcludeVersion -OutputDirectory $tempFolder -ConfigFile $nugetConfigPath
+try {
+    # Ensure the module is not already installed
+    if (!(Get-Module -ListAvailable -Name $moduleName)) {
+        # Save the NuGet package containing the module to a temporary folder
+        $tempFolder = New-Item -ItemType Directory -Path (Join-Path $env:TEMP ([System.Guid]::NewGuid().ToString()))
+        $nugetConfigPath = "$pathNuget\nuget.config"
+        & "nuget.exe" install $moduleName -ExcludeVersion -OutputDirectory $tempFolder -ConfigFile $nugetConfigPath
     
-    # Import the module from the saved NuGet package
-    $moduleFolder = Get-ChildItem -Path $tempFolder -Directory | Select-Object -First 1
-    $modulePath = Join-Path $moduleFolder.FullName "$moduleName.psm1"
-    Import-Module -Name $modulePath -Verbose
-}
+        # Import the module from the saved NuGet package
+        $moduleFolder = Get-ChildItem -Path $tempFolder -Directory | Select-Object -First 1
+        $modulePath = Join-Path $moduleFolder.FullName "$moduleName.psm1"
+        Import-Module -Name $modulePath -Verbose
+    }
 
-Get-CatFact
+    Get-CatFact    
+}
+catch {
+    $message = $_
+    Write-Error "exception happened! $message"   
+}
