bb7390b8157e1edc99b8f9f4302a133120573380
LongjunXU <xulongjun.fr@gmail.com>
2023-04-17 23:28:22 +0200

    test

--- Install-Module.ps1
+++ Install-Module.ps1
@@ -1,11 +1,25 @@
-$ErrorActionPreference = 'Stop'
-$moduleName = "KpInfo"
+param 
+(
+    $pathNuget,
+    $moduleName = "KpInfo"
+)
+
+# $pathNuget = $PSScriptRoot
+
+# First time
+# Write-Host "Downloading nuget.exe..."
+# Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile 'C:/nuget/nuget.exe'
+# Write-Host "Adding nuget.exe to PATH..."
+# $env:PATH += ";C:/nuget/"
+
+# Uninstall-Module -Name "KpInfo"  -AllVersions
+# Remove-Module -Name "KpInfo"
 
 # Ensure the module is not already installed
 if (!(Get-Module -ListAvailable -Name $moduleName)) {
     # Save the NuGet package containing the module to a temporary folder
     $tempFolder = New-Item -ItemType Directory -Path (Join-Path $env:TEMP ([System.Guid]::NewGuid().ToString()))
-    $nugetConfigPath = "$(Build.Repository.LocalPath)\nuget.config"
+    $nugetConfigPath = "$pathNuget\nuget.config"
     & "nuget.exe" install $moduleName -ExcludeVersion -OutputDirectory $tempFolder -ConfigFile $nugetConfigPath
     
     # Import the module from the saved NuGet package
@@ -13,3 +27,5 @@ if (!(Get-Module -ListAvailable -Name $moduleName)) {
     $modulePath = Join-Path $moduleFolder.FullName "$moduleName.psm1"
     Import-Module -Name $modulePath -Verbose
 }
+
+Get-CatFact
